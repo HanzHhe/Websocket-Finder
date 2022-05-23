d6d706af791d6793ae42eddb847fb72c67b99347
/Brainerror <98905505+HanzHhe@users.noreply.github.com>
2022-05-23 22:11:43 +0700

    Add files via upload

--- wsfind.py
+++ wsfind.py
@@ -20,7 +20,6 @@ os.system('clear')
 print("Timeout: 10")
 print('Author: \033[1;31m./Brainerror\n\033[0;0mFacebook: \033[1;31mhttps://www.facebook.com/hanz.ii.180\n\033[0;0m')
 
-#try:
 for dom in host.read().splitlines():
   try:
     ip = socket.gethostbyname( dom )
@@ -51,30 +50,30 @@ for dom in host.read().splitlines():
   except:
      sock.close()
      pass
+  payload = "GET wss://{}/ HTTP/1.1\r\nHost: {}\r\nUpgrade: websocket\r\n\r\n".format(dom, ssh)
   sock = socket.socket()
   cont = ssl.SSLContext()
   sl = cont.wrap_socket(sock, server_hostname=dom)
+  sl.do_handshake_on_connect
   sl.settimeout(7)
   port = 443
-  try:
-    sl.connect_ex((dom, port))
-    sl.getpeercert(True)
-  except:
-    print(f"Host: {dom} Tidak Merespon")
-  try:
-      sl.send(payload.encode())
-      data = sl.recv(1024).decode()
-      if '101 S' in data:
-         prit(f"\033[1;32mBug Ditemukan: {dom}\nSocket: {ip}:{port}\nSni: {host}\n\033[0;0m")
+  sl.connect_ex((dom, port))
+  sl.getpeercert()
+  sl.context.load_default_certs()
+#  except:
+#    print(f"Host: {dom} Tidak Merespon")
+#  try:
+  sl.send(payload.encode())
+  data = sl.recv(1024).decode()
+  if '101 S' in data:
+         print(f"\033[1;32mBug Ditemukan: {dom}\nSocket: {ip}:{port}\nSni: {dom}\n\033[0;0m")
          print(data)
          if 'Bug' in hsr.read():
            hsa.write(f"Bug: {dom}\nSocket: {ip}:{port}\nPayload: GET / HTTP/1.1[]Host: isi ssh klean[crlf]Upgrade: websocket[crlf][crlf]\n\nYNTKTS: {data}\n\n")
          else:
            hasil.write(f"Bug: {dom}\nSocket: {ip}:{port}\nPayload: GET / HTTP/1.1[crlf]Host: isi ssh klean[crlf]Upgrade: websocket[crlf][crlf]\nYNTKTS: {data}")
-      else:
+  else:
          print(f"\033[1;31mBug WSS Tidak Ditemukan Di Host: {dom}\nSocket: {ip}:{port}\n\033[0;0m")
          print(data)
-  except:
-      print(f"\033[1;31mHost SNI Dari: {dom} Tidak Ditemukan\n\033[0;0m")
-#except:
-# exi
+#  except:
+#      print(f"\033[1;31mHost SNI Dari: {dom} Tidak Ditemukan\n\033[0;0m")
